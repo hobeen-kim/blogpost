6745dc2da58c01c3518118896eca67e1d3d9b68e
hobeen-kim <sksjsksh32@gmail.com>
2025-12-15 22:45:23 +0900

    feat: buffer - init

---
+++ buffer/src/main/kotlin/com/hobeen/buffer/BufferApplication.kt
@@ -0,0 +1,11 @@
+package com.hobeen.buffer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BufferApplication
+
+fun main(args: Array<String>) {
+    runApplication<BufferApplication>(*args)
+}
---
+++ buffer/src/test/kotlin/com/hobeen/buffer/BufferApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hobeen.buffer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BufferApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
