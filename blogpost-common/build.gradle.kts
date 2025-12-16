6b859e3afbb6fedd89b9729a3bd105f00bd2b8b9
hobeen-kim <sksjsksh32@gmail.com>
2025-12-16 14:37:20 +0900

    fix: gitflow

--- blogpost-common/build.gradle.kts
+++ blogpost-common/build.gradle.kts
@@ -14,7 +14,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/<OWNER>/<REPO>")
+            url = uri("https://maven.pkg.github.com/hobeen-kim/blogpost-collector")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
