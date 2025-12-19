2d2d41c0ca33867fd2227c517967e2fa9046ae83
hobeen-kim <sksjsksh32@gmail.com>
2025-12-19 14:21:55 +0900

    feat: common - autoconfiguration 설정

---
+++ blogpost-common/src/main/kotlin/blogpostcommon/alarm/config/AlarmAutoConfiguration.kt
@@ -0,0 +1,19 @@
+package com.hobeen.blogpostcommon.alarm.config
+
+import com.hobeen.blogpostcommon.alarm.AlarmService
+import com.hobeen.blogpostcommon.alarm.slack.SlackAlarmService
+import com.hobeen.blogpostcommon.alarm.slack.SlackProperties
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+
+@AutoConfiguration
+@EnableConfigurationProperties(SlackProperties::class)
+class AlarmAutoConfiguration {
+    @Bean
+    @ConditionalOnProperty(prefix = "alarm.slack", name = ["enabled"], havingValue = "true")
+    fun slackAlarmService(props: SlackProperties): AlarmService {
+        return SlackAlarmService(props)
+    }
+}
--- blogpost-common/src/main/kotlin/blogpostcommon/alarm/slack/SlackAlarmService.kt
+++ blogpost-common/src/main/kotlin/blogpostcommon/alarm/slack/SlackAlarmService.kt
@@ -11,8 +11,6 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-@Component
-@ConditionalOnProperty(prefix = "alarm.slack", name = ["enabled"], havingValue = "true")
 class SlackAlarmService(
     private val slackProperties: SlackProperties,
 ): AlarmService {
