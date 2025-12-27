9e57920ae825b6b78ca859d2cd5e30b98b08a12a
hobeen-kim <sksjsksh32@naver.com>
2025-12-27 21:24:31 +0900

    fix: apiserver - 로그인 여부 확인 로직 수정

--- apiserver/src/main/kotlin/com/hobeen/apiserver/util/auth/AuthUtil.kt
+++ apiserver/src/main/kotlin/com/hobeen/apiserver/util/auth/AuthUtil.kt
@@ -1,11 +1,15 @@
 package com.hobeen.apiserver.util.auth
 
 import com.hobeen.apiserver.util.exception.AuthenticationException
+import org.springframework.security.authentication.AnonymousAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.jwt.Jwt
 
 fun isLogin(): Boolean {
-    return SecurityContextHolder.getContext()?.authentication?.isAuthenticated == true
+    val authentication = SecurityContextHolder.getContext()?.authentication
+
+    return if(authentication == null || authentication is AnonymousAuthenticationToken) false
+    else true
 }
 
 fun authUserId(): String {
